96c9e21e42e1e639404a0d90297850b8c847bbc1
Kims-club98 <naxen5611@naver.com>
2025-12-29 17:18:22 +0900

    251229 17:20

--- src/main/java/com/mvc/ActionSupport.java
+++ src/main/java/com/mvc/ActionSupport.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 // -> *. 이런 요청에 대해서는 내가 다 받을게
 // .ks 확장자가 ks로 끝나는 모든 요청에 대해서는 내가 가로챈다.
@@ -53,9 +54,51 @@ public class ActionSupport extends HttpServlet {
 	log.info("end : "+end); //15
 	command = command.substring(0,end);
 	log.info(command); // -> dept/deptInsert
-	String [] path = command.split("/");
-	for(String s:path) {
-		log.info(s);
+	// 요청받은 내용을 해당 업무에 대응되는 XXXController 전달하고 응답처리를 위한 후처리를 해야 한다.
+	Object obj = null;// ModelAndView & String
+	// viewName -> return "redirect:./deptInsertOk.jsp"
+	// viewName -> return "forward:./deptInsertOk.jsp"
+	// viewName -> return new ModelAndView()
+	// >> insert here - HandlerMapping 연결하기
+	obj = HandlerMapping.getController(command);
+	String [] pageMove = null;
+	if(obj != null) {
+		ModelAndView mav = null;
+		//Object에는 2가지 Type이 있음. String(Insert, Update, Delete), ModelAndView(slect)
+		if(obj instanceof String) {
+			log.info("return 타입이 String 일때 ▼");
+			if(((String)obj).contains("/")) {
+				log.info("내 안에 콜론(:)이 있다.");
+				PageMove = obj.toString().split("/");
+				}
+			else {
+				log.info("내 안에 슬래쉬(/)도 업고 콜론(:)도 없음");
+			}
+			} // Controller 요청 메서드의 return 타입이 String인 경우 end	
+			else if(obj instanceof ModelAndView) {
+				log.info("return 타입이 ModelAndView 일때 ▼");
+				
+				mav = (ModelAndView)obj;
+				pageMove = new String[2];
+				pageMove [0] = "modelAndView";
+				pageMove [1] = mav.getViewName();
+			}//컨트롤러 요청 메서드의 리턴 타입이 ModelAndView인 경우 end
+		}// end of obj가 Null이 아닐 때 ==> NullPointException을 예방하기 위한 Code!
+	//////////////////////////////////////////////////////////////////
+	/// /////////////////////[[ ViewResolver ]] //////////////////////
+	if(pageMove != null && pageMove.length == 2) {
+		log.info("pageMove 배열의 원소 갯수가 2개일 때");
+		String path = pageMove[1]; // 각 Controller 클래싕 메서드가 정한 페이지의 이름
+		if("redirect".equals(pageMove[0])) {
+			res.sendRedirect(pageMove[1])){
+				RequestDispatcher view = req.getRequestDispatcher("/"+path+".jsp");
+									req.getRequestDispatcher("/WEB-INF/views/"+path+".jsp");
+					view.forward(req, res);				
+			}
+			// Spring boot 에서 요청에 대한 응답 URL를 완성해주는 ViewResolver클래스가 제공됨.
 		}
+	}
+	
+	
 	}
 }
